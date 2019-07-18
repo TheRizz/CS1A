f57a3219a6833635fdb0919b5b3f8c24c05b6bf1
TheRizz <TheRizz@users.noreply.github.com>
2019-07-17 17:29:52 -0700

    Removed unnecessary test Folder

--- test/html/search/classes_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['newitem',['NewItem',['../class_new_item.html',1,'']]],
-  ['newmember',['NewMember',['../class_new_member.html',1,'']]]
-];
--- test/html/search/classes_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['purchase',['Purchase',['../class_item_1_1_purchase.html',1,'Item']]]
-];
